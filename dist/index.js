aace1d4c105e2a07066da20a95d9ca75c7d788ec
afeiship <1290657123@qq.com>
2021-02-01 10:32:46 +0800

    Release 1.0.0

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,12 @@
  * description: Strip html to text for next.
  * homepage: https://github.com/afeiship/next-html2text
  * version: 1.0.0
- * date: 2020-11-20 18:46:51
+ * date: 2021-02-01 10:32:38
  * license: MIT
  */
 
 (function () {
-  var global = global || this || window || Function('return this')();
+  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
 
   nx.html2text = function (inHtml) {
